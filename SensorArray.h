b0e6544326427742f0e934ae28d3df4e84b0c00a
Erick Sanchez <esericksanc@gmail.com>
2016-12-31 13:03:12 -0800

    rewrote SensorsArray in cpp file

--- SensorArray.h
+++ SensorArray.h
@@ -1,51 +1,10 @@
 #pragma once
 
+class SensorsArray;
+
 class SensorArray
 {
 	int N_SENSORS;
-	struct sensor {
-		int pin;
-		int position;
-		//Sensor *sensor;
-		sensor *next;
-	};
-	class SensorsArray {
-		sensor *head, *tail;
-	public:
-		SensorsArray() {
-			head = tail = nullptr;
-		}
-
-		~SensorsArray() {
-			sensor *curr = head;
-			while (curr != nullptr) {
-				sensor *destory = curr;
-				curr = destory->next;
-				delete destory;
-			}
-		}
-
-		//inserts a new node at the tail vs the head
-		void insert(int pinValue, float posValue) {
-			sensor *instance = new sensor();
-			instance->pin = pinValue;
-			instance->position = posValue;
-			tail->next = instance;
-
-			tail = instance;
-		}
-
-		//starting at 0 as the first node. Iterates starting at the head
-		sensor* at(int indexValue) {
-			sensor *node = head;
-			while (indexValue > 0) {
-				node = node->next;
-				indexValue -= 1;
-			}
-
-			return node;
-		}
-	};
 	SensorsArray sensors;
 public:
 	SensorArray();
