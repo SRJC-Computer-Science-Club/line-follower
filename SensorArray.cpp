76906ac2d9fdb9ba8495f668ec9b0f7f84cc1c13
fuzzybear3 <stevenguido.guido@gmail.com>
2016-12-30 20:51:39 -0800

    forgot to save files before commit...

---
+++ SensorArray.cpp
@@ -0,0 +1,25 @@
+
+
+
+SensorArray::SensorArray(int pins[])
+	: nSensors(nSensors)
+{
+	std::vector<Sensor*> sensors;
+
+
+
+	for (int i = 0; i < nSensors; i++)
+	{
+		//Init each sensor
+	}
+}
+
+
+void prepSensors()
+{
+	for (int i = 0; i < X; i++)
+	{
+
+	}
+
+}
--- SensorArray.h
+++ SensorArray.h
@@ -1,1 +1,22 @@
 #pragma once
+
+#include "Sensor.h"
+
+
+namespace LFRobot
+{
+
+	class SensorArray
+	{
+	public:
+
+		SensorArray(int pins[]);
+		void prepSensors();
+
+
+	private:
+		int pins[];
+
+	};
+
+}
