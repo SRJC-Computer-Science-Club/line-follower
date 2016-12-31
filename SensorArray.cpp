e65783934f89f4c328feb59da2b8e6aadccbe477
Erick Sanchez <esericksanc@gmail.com>
2016-12-31 13:01:09 -0800

    SensorsArray
    commented values for the Sensor class

--- SensorArray.cpp
+++ SensorArray.cpp
@@ -13,19 +13,35 @@
 
 SensorArray::SensorArray()
 {
+	sensors = SensorsArray();
 }
 
 SensorArray::SensorArray(int pins[], int nPins)
 {
+	N_SENSORS = nPins;
+	for (int pin = 0; pin < N_SENSORS; pin++)
+	{
+		pinMode(pin, INPUT);
+		float pos = 2 / N_SENSORS * pin;
+		sensors.insert(pin, pos);
+	}
 }
 
 
 SensorArray::~SensorArray()
 {
+	sensors.~SensorsArray();
 }
 
 
 double SensorArray::error()
 {
-	return 0;
+	float sum1 = 0, sum2 = 0;
+	for (int sensor = 0; sensor < N_SENSORS; sensor++)
+	{
+		//sum1 += sensors.at(sensor)->position * sensors.at(sensor)->sensor.value();
+		//sum2 += sensors.at(sensor)->sensor.value();
+	}
+
+	return sum1 / sum2;
 }
