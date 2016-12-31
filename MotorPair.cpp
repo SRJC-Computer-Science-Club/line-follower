feb99fb08743ebb657382db81661bfc8ebc5ae10
Erick Sanchez <esericksanc@gmail.com>
2016-12-31 11:18:54 -0800

    define motor pair class

--- MotorPair.cpp
+++ MotorPair.cpp
@@ -4,9 +4,19 @@
 
 MotorPair::MotorPair()
 {
+	left = new motor();
+	right = new motor();
 }
 
 
 MotorPair::~MotorPair()
 {
+	delete left, right;
+}
+
+
+void MotorPair::moveForward(float speed, float offset)
+{
+	left->setSpeed((1 + speed)*offset + speed);
+	right->setSpeed(-(1 + speed)*offset + speed);
 }
--- MotorPair.h
+++ MotorPair.h
@@ -6,16 +6,8 @@ class MotorPair
 {
 	motor *left, *right;
 public:
-	MotorPair() {
-		left = new motor();
-		right = new motor();
-
-	}
+	MotorPair();
 	~MotorPair();
-	void moveForward(float speed, float offset) {
-		left->setSpeed((1 + speed)*offset + speed);
-		right->setSpeed(-(1 + speed)*offset + speed);
-
-	}
+	void moveForward(float speed, float offset);
 };
 
