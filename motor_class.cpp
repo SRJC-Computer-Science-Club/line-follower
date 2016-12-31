6d31a848c0f00356672154227e196faab01f4a26
Erick Sanchez <esericksanc@gmail.com>
2016-12-31 11:12:40 -0800

    adjusted map function
    using the data types we need and capping the input value, x, from x to
    in_max

--- motor_class.cpp
+++ motor_class.cpp
@@ -1,4 +1,9 @@
 #include "motor_class.h"
+#include "Math.h"
 
+int map(float x, float in_min, float in_max, float out_min, float out_max)
+{
+	return ceil((fmin(x, in_max) - in_min) * (out_max - out_min) / (in_max - in_min) + out_min);
+}
 
 
