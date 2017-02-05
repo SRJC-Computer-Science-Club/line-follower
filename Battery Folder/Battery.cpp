dff59ce99bce776cb66382e242658baee57fa298
Austin <meyerforge@gmail.com>
2017-02-04 20:56:05 -0800

    Battery Class v0.0.0
    I have know idea what I am doing and I don't know why!!!

---
+++ Battery Folder/Battery.cpp
@@ -0,0 +1,12 @@
+#include "Battery.h"
+
+
+
+Battery::Battery()
+{
+}
+
+
+Battery::~Battery()
+{
+}
---
+++ Battery Folder/Battery.h
@@ -0,0 +1,22 @@
+#pragma once
+// Austin Meyer
+// Project - line_follower
+// Date Created 2/4/2017
+// Last Opened 2/4/2017
+
+const int PIN_NUMBER = 15;
+const int BATTERY_N_READINGS = 10;
+const int BATTERY_MAX_DESCREPENCY = 5;
+
+class Battery
+{
+	private:
+		int pin;
+		int powerAverage[];
+	public:
+		Battery ();
+		~Battery ();
+		bool checkBattery ();
+
+};
+
