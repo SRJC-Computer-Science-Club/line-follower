0cfca8e4f09a54710338cb46051cb4f7997d431e
Josh Alan <thedatasmith1@gmail.com>
2017-04-01 15:56:24 -0700

    AveragingQueue Documentation

--- AveragingQueue.h
+++ AveragingQueue.h
@@ -4,22 +4,42 @@
 
 namespace LFRobot
 {
+	/*
+	AveragingQueue functions as a normal queue, with two added features.
+
+	1. It keeps track of the average of all the values in the queue, 
+	so that it can be found in constant time.
+
+	2. It has a maximum size, called capacity. When the capacity is reached,
+	the oldest element is removed from the queue and from the average.
+
+	If capcity is zero, then the queue has unlimited size.
+	*/
 	template <class T>
 	class AveragingQueue
 	{
 	public:
-
+		/*
+		Initializes an empty queue with unlimited size.
+		*/
 		AveragingQueue()
 		{
 			capacity = 0;
 		}
 
+		/*
+		Initializes an empty queue with a given maximum size.
+		*/
 		AveragingQueue(size_t inCapacity)
 		{
 			capacity = inCapacity;
 		}
 
-		push(T element)
+		/*
+		Pushes a new element onto the queue and into the average.
+		If the queue is full, the oldest element is removed.
+		*/
+		void push(T element)
 		{
 			sum += element;
 			q.push(element);
@@ -31,6 +51,10 @@ namespace LFRobot
 			}
 		}
 
+		/*
+		Removes the oldest element from the queue and from the average.
+		That element is returned.
+		*/
 		T pop()
 		{
 			T temp = q.front();
@@ -38,6 +62,9 @@ namespace LFRobot
 			return temp;
 		}
 
+		/*
+		Returns the average of all the elements in the queue.
+		*/
 		T getAverage()
 		{
 			if (q.empty())
@@ -48,16 +75,25 @@ namespace LFRobot
 			return sum / q.size();
 		}
 
+		/*
+		Returns the number of elements currently in the queue.
+		*/
 		size_t getSize()
 		{
 			return q.size();
 		}
 
+		/*
+		True if the capacity is limited and the size is equal to it.
+		*/
 		bool isFull()
 		{
 			return capacity > 0 && q.size() == capacity;
 		}
 
+		/*
+		True if there are no elements in the queue.
+		*/
 		bool isEmpty()
 		{
 			return q.empty();
